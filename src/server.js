9e8561c6df5e118296c9af0789f38f41fd3ee6c7
Yaroslav Yekasov <fngpr.56@gmail.com>
2026-04-17 03:02:38 +0300

    chore(db): disable automatic schema bootstrap

--- src/server.js
+++ src/server.js
@@ -6,12 +6,10 @@ import { env } from "./config/env.js";
 import { registerSocketHandlers } from "./sockets/index.js";
 import { logger } from "./utils/logger.js";
 import { testDbConnection } from "./config/db.js";
-import { initializeAuthSchema } from "./services/auth-schema.service.js";
 
 async function bootstrap() {
   // Make sure the database is ready before the server accepts requests.
   await testDbConnection();
-  await initializeAuthSchema();
 
   if (env.sessionSecret === "change-me-in-production") {
     logger.warn("SESSION_SECRET is using the development fallback value");
